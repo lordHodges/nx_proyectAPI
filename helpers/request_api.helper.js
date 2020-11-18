be74aee184353ddb82d2bcb1ebbf49cb60c0c18b
victor <hodgestroncoso@gmail.com>
2020-11-18 10:04:35 -0300

    probando servico rentaccar

--- helpers/request_api.helper.js
+++ helpers/request_api.helper.js
@@ -2,14 +2,18 @@ const request = require("request");
 const fs = require("fs");
 
 const cert = fs.readFileSync("fullchain4.pem");
-const cert = fs.readFileSync("privkey4.pem");
+const key = fs.readFileSync("privkey4.pem");
 class RequestApiHelper {
 	make_API_call(url) {
 		return new Promise((resolve, reject) => {
-			request(url, { json: true, ca: cert, key }, (err, res, body) => {
-				if (err) reject(err);
-				resolve(body);
-			});
+			request(
+				url,
+				{ json: true, ca: cert, key: key, strictSSL: true },
+				(err, res, body) => {
+					if (err) reject(err);
+					resolve(body);
+				}
+			);
 		});
 	}
 }
