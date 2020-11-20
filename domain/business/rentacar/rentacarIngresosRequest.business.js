742efd93b6eeb6f4b20fc86064503986801a1597
victor <hodgestroncoso@gmail.com>
2020-11-20 14:09:43 -0300

    porbando recuperacion

--- domain/business/rentacar/rentacarIngresosRequest.business.js
+++ domain/business/rentacar/rentacarIngresosRequest.business.js
@@ -7,6 +7,36 @@ class RentacarIngresosRequestBusiness extends BaseBusiness {
 	}
 	async getArriendos() {
 		const arriendos = await this._rentacarService.getArriendos();
+
+		const arriendosEstado = await this.getEstadoPago(arriendos);
+
+		return arriendosEstado;
+	}
+
+	///
+	//!manipulando el objeto 13
+	//?,metodo para injectar variable con el valor total del arriendo.
+	async getTotalArriendo() {}
+
+	//?,metodo para injectar variable con el valor de estado pago del arriendo.
+	async getEstadoPago(arriendos) {
+		let count = 0;
+
+		arriendos["data"].forEach((arriendo) => {
+			arriendo["pagosArriendos"].forEach((pagoArriendo) => {
+				pagoArriendo["pagos"].forEach((pago) => {
+					if (pago["estado_pago"] == "PENDIENTE") {
+						count++;
+					}
+				});
+			});
+			if (count > 0) {
+				arriendo["estado_pago"] = "PENDIENTE";
+			} else {
+				arriendo["estado_pago"] = "PAGADO";
+			}
+			count = 0;
+		});
 		return arriendos;
 	}
 }
