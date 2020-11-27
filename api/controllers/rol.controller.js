bd81e8d8f80a755d639dac77036e6122e610286d
victor <hodgestroncoso@gmail.com>
2020-11-27 10:51:32 -0300

    cambios rol mapper

--- api/controllers/rol.controller.js
+++ api/controllers/rol.controller.js
@@ -2,50 +2,47 @@ const mapper = require("automapper-js");
 const { RolDto } = require("../dtos");
 
 class RolController {
-  constructor({ RolService }) {
-    this._rolService = RolService;
-  }
-  async getRoles(req, res) {
-    let roles = await this._rolService.getAll();
-    roles = roles.map((rol) => mapper(RolDto, rol));
-    return res.send(roles);
-  }
+	constructor({ RolService }) {
+		this._rolService = RolService;
+	}
+	async getRoles(req, res) {
+		let roles = await this._rolService.getAll();
+		return res.send(roles);
+	}
 
-  async getRol(req, res) {
-    const { id } = req.params;
-    let rol = await this._rolService.get(id);
-    if (!rol) {
-      return res.status(404).send();
-    }
-    rol = mapper(RolDto, rol);
-    return res.send({
-      payload: rol,
-    });
-  }
+	async getRol(req, res) {
+		const { id } = req.params;
+		let rol = await this._rolService.get(id);
+		if (!rol) {
+			return res.status(404).send();
+		}
+		return res.send({
+			payload: rol,
+		});
+	}
 
-  async createRol(req, res) {
-    const { body } = req;
-    const createdRol = await this._rolService.create(body);
-    const rol = mapper(RolDto, createdRol);
-    return res.status(201).send({
-      payload: rol,
-    });
-  }
+	async createRol(req, res) {
+		const { body } = req;
+		const rol = await this._rolService.create(body);
+		return res.status(201).send({
+			payload: rol,
+		});
+	}
 
-  async updateRol(req, res) {
-    const { body } = req;
-    const { id } = req.params;
+	async updateRol(req, res) {
+		const { body } = req;
+		const { id } = req.params;
 
-    await this._rolService.update(id, body);
-    return res.status(204).send();
-  }
+		await this._rolService.update(id, body);
+		return res.status(204).send();
+	}
 
-  async deleteRol(req, res) {
-    const { id } = req.params;
+	async deleteRol(req, res) {
+		const { id } = req.params;
 
-    await this._rolService.delete(id);
-    return res.status(204).send();
-  }
+		await this._rolService.delete(id);
+		return res.status(204).send();
+	}
 }
 
 module.exports = RolController;
